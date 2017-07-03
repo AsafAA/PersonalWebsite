e04e70285e47a7327a8a514b4f380572eeb55e32
AsafAA <asafaa32@gmail.com>
2017-07-03 11:45:06 -0700

    Reverting all the way

--- src/App.js
+++ src/App.js
@@ -1,54 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-var Carousel = require('react-responsive-carousel').Carousel;
-var x = require('react-responsive-carousel/lib/styles/carousel.css')
 
 class App extends Component {
   render() {
     return (
       <div className="app" style={{display:"flex", justifyContent:"center", fontFamily:"HelveticaNeue-Light", paddingLeft:"20px", paddingRight:"20px"}}>
-        <div className="wrapper" style={{width:"1000px", alignItems:"center"}}>
+        <div className="wrapper" style={{width:"900px", alignItems:"center"}}>
 
           <div className="header" style={{display:"flex", justifyContent:"center"}}>
             <h1 style={{fontSize:"50px", margin:"30px 0px 20px 0px", color:"#333333"}}>ASAF AVIDAN ANTONIR</h1>
           </div>
 
           <div className="navBar" style={{color:"#666666", display:"flex", justifyContent:"center", borderBottomStyle:"solid", borderBottomWidth:"1px", borderBottomColor:"#CCCCCC"}}>
-            <a style={{margin:"0px 10px 15px 10px", fontSize:"18px"}}>About</a>
-            <a style={{margin:"0px 10px 15px 10px", fontSize:"18px"}}>Projects</a>
-            <a style={{margin:"0px 10px 15px 10px", fontSize:"18px"}}>Experience</a>
-            <a style={{margin:"0px 10px 15px 10px", fontSize:"18px"}}>Contact</a>
+            <a style={{margin:"0px 10px 15px 10px"}}>About</a>
+            <a style={{margin:"0px 10px 15px 10px"}}>Projects</a>
+            <a style={{margin:"0px 10px 15px 10px"}}>Experience</a>
+            <a style={{margin:"0px 10px 15px 10px"}}>Contact</a>
           </div>
 
-          <div className="topCard" style={{ borderBottomStyle:"solid", borderBottomWidth:"1px", borderBottomColor:"#CCCCCC", paddingTop:"50px", paddingBottom:"50px"}}>
+          <div className="topCard" style={{ borderBottomStyle:"solid", borderBottomWidth:"1px", borderBottomColor:"#CCCCCC"}}>
             <div className="imgSection" style={{flexBasis:"35%", display:"flex", alignItems:"center"}}>
-              <img className="profilePicture" src="./AsafIllumio.jpg" style={{width:"100%", borderRadius:"50%"}}/>
+              <img src="./AsafIllumio.jpg" style={{width:"100%", borderRadius:"50%"}}/>
             </div>
-            <div className="contentSection" style={{flexBasis:"60%"}}>
-              <h1 style={{margin:"0px", color:"#333333", fontSize:"34px"}}>HI, MY NAME IS ASAF.</h1>
-              <p style={{fontFamily:"Helvetica-Bold", color:"#555555", fontSize:"18px"}}>I&#39;m an engineer, entrepreneur, and ping pong enthusiast.</p>
-              <p style={{color:"#666666", fontSize:"18px"}}>At a young age I moved from Israel to Silicon Valley for my father&#39;s startup, quickly inheriting the entrepreneurial bug and love for technology.</p>
-              <p style={{color:"#666666", fontSize:"18px"}}>I studied Electrical Engineering and Computer Science at UC Berkeley, with a focus in graduating ASAP. On campus, I was involved in the hackathon community as director of the first Cal Hacks, and the Israeli community through my work in Mishelanu.</p>
-              <p style={{color:"#666666", fontSize:"18px"}}>I&#39;ve since picked up experience at amazing companies like Illumio, LinkedIn, and Microsoft, and currently spend my days exploring solutions in artificial intelligence.</p>
+            <div className="contentSection" style={{flexBasis:"60%", paddingTop:"50px", paddingBottom:"50px"}}>
+              <h1 style={{margin:"0px", color:"#333333"}}>HI, MY NAME IS ASAF.</h1>
+              <p style={{fontFamily:"Helvetica-Bold", color:"#555555"}}>I&#39;m an engineer, entrepreneur, and ping pong enthusiast.</p>
+              <p style={{color:"#666666"}}>At a young age I moved from Israel to Silicon Valley for my father&#39;s startup, quickly inheriting the entrepreneurial bug and love for technology. I studied Electrical Engineering and Computer Science at UC Berkeley, with a focus in graduating ASAP. On campus, I was heavily involved in the hackathon community as director of the first Cal Hacks, and the Israeli community through my work in Mishelanu. I&#39;ve sinced picked up experience at amazing companies like Illumio, LinkedIn, and Microsoft, and currently spend my days exploring solutions in artificial intelligence.</p>
               <div className="socialLinks" style={{display:"flex", justifyContent:"center"}}>
-                <a href="https://www.linkedin.com/in/asaf-avidan-antonir-95888558"><img src="./LinkedInSquare.png" style={{width:"40px", height:"40px", margin:"5px 15px 0px 15px", borderRadius:"4px"}}/></a>
-                <a href="https://www.facebook.com/asaf.avidanantonir"><img src="./FacebookIcon.png" style={{width:"40px", height:"40px", margin:"5px 15px 0px 15px", borderRadius:"4px"}}/></a>
-                <a href="https://github.com/AsafAA"><img src="./GitHubSquare.svg" style={{width:"40px", height:"40px", margin:"5px 15px 0px 15px", borderRadius:"4px"}}/></a>
+                <a href="https://www.linkedin.com/in/asaf-avidan-antonir-95888558"><img src="./LinkedInSquare.png" style={{width:"40px", height:"40px", margin:"5px 15px 5px 15px", borderRadius:"4px"}}/></a>
+                <a href="https://www.facebook.com/asaf.avidanantonir"><img src="./FacebookIcon.png" style={{width:"40px", height:"40px", margin:"5px 15px 5px 15px", borderRadius:"4px"}}/></a>
+                <a href="https://github.com/AsafAA"><img src="./GitHubSquare.svg" style={{width:"40px", height:"40px", margin:"5px 15px 5px 15px", borderRadius:"4px"}}/></a>
               </div>
             </div>
           </div>
 
         {/*TODO: turn these into components*/}
-          <div className="projects" style={{paddingTop:"50px", paddingBottom:"40px", borderBottomStyle:"solid", borderBottomWidth:"1px", borderBottomColor:"#CCCCCC"}}>
-            <h2 style={{margin:"0px", fontSize:"34px", color:"#333333"}}>PROJECTS</h2>
+          <div className="projects" style={{paddingTop:"50px", paddingBottom:"50px", borderBottomStyle:"solid", borderBottomWidth:"1px", borderBottomColor:"#CCCCCC"}}>
+            <h2 style={{margin:"0px", fontSize:"30px", color:"#333333"}}>PROJECTS</h2>
 
-            <div className="project" style={{marginTop:"10px", marginBottom:"40px"}}>
+            <div className="project" style={{marginTop:"10px", marginBottom:"20px"}}>
               <div className="projectContent" style={{flexBasis:"50%"}}>
-                <h3 style={{fontSize:"24px", color:"#333333"}}>STEADY SQUARE</h3>
-                <p style={{fontFamily:"Helvetica-Bold", color:"#555555", fontSize:"18px"}}>An app for weighing objects on your phone&#39;s screen.</p>
-                <p style={{color:"#666666", fontSize:"18px"}}>When apple introduced 3d touch, I wanted to turn my iPhone into a functioning scale. When others&#39; attempts were rejected from the app store, I built a game that could be secretly used to do it.</p>
-                <p style={{color:"#666666", fontSize:"18px"}}>After nearly a million video views and over 100,000 downloads in two days, I got a nice call from apple and the app and video had to go. Here is the video quietly re-uploaded!</p>
+                <h3 style={{fontSize:"20px", color:"#333333"}}>STEADY SQUARE</h3>
+                <p style={{fontFamily:"Helvetica-Bold", color:"#555555"}}>An app for weighing objects on your phone&#39;s screen.</p>
+                <p>When apple introduced 3d touch, I wanted to turn my iPhone into a functioning scale. When others&#39; attempts were rejected from the app store, I built a Flappy Bird like game that could be secretly used to do it.</p>
+                <p>After nearly a million video views and over 100,000 downloads in two days, I got a nice call from apple and the app and video had to go. Here is the video quietly re-uploaded!</p>
                 <div className="pressLinks">
                   <a href="http://www.businessinsider.com/how-to-turn-your-iphone-into-a-working-scale-2015-12"><img src="./BusinessInsiderLogo.png" style={{height:"18px", margin:"5px 30px 5px 0px"}}/></a>
                   <a href="http://mashable.com/2015/12/12/5-cant-miss-apps-photomath-steady-square/#XnUd6XR8laq3"><img src="./MashableLogo.svg" style={{height:"18px", margin:"5px 30px 5px 0px"}}/></a>
@@ -66,10 +62,10 @@ class App extends Component {
 
             <div className="project" style={{marginTop:"10px", marginBottom:"20px"}}>
               <div classNAme="projectContent" style={{flexBasis:"50%"}}>
-                <h3 style={{fontSize:"24px", color:"#333333"}}>DITTO KEYBOARD</h3>
-                <p style={{fontFamily:"Helvetica-Bold", color:"#555555", fontSize:"18px"}}>A keyboard for sending commonly used text snippets.</p>
-                <p style={{color:"#666666", fontSize:"18px"}}>Whether it’s texting, dating, business, or just regular daily communication, we tend to repeat ourselves quite often.</p>
-                <p style={{color:"#666666", fontSize:"18px"}}>With Ditto, save your most commonly used texts, phrases, and templates, and access them easily from any app!</p>
+                <h3 style={{fontSize:"20px", color:"#333333"}}>DITTO KEYBOARD</h3>
+                <p style={{fontFamily:"Helvetica-Bold", color:"#555555"}}>A keyboard for sending commonly used text snippets.</p>
+                <p>Whether it’s texting, dating, business, or just regular daily communication, we tend to repeat ourselves quite often.</p>
+                <p>With Ditto, save your most commonly used texts, phrases, and templates, and access them easily from any app!</p>
                 <div className="pressLinks">
                   <a href="https://www.producthunt.com/posts/ditto-keyboard"><img src="./ProductHuntLogo.png" style={{height:"18px", margin:"5px 30px 5px 0px"}}/></a>
                   <a href="https://www.yahoo.com/tech/7-awesome-paid-iphone-apps-free-downloads-now-145235184.html"><img src="./YahooLogo.png" style={{height:"18px", margin:"5px 30px 5px 0px"}}/></a>
@@ -87,95 +83,26 @@ class App extends Component {
 
           </div>
 
-          <div className="experience" style={{paddingTop:"50px", paddingBottom:"50px", borderBottomStyle:"solid", borderBottomWidth:"1px", borderBottomColor:"#CCCCCC"}}>
-            <h2 style={{margin:"0px", fontSize:"34px", color:"#333333"}}>EXPERIENCE</h2>
-
+          <div className="experience" style={{height:"500px", borderBottomStyle:"solid", borderBottomWidth:"1px", borderBottomColor:"#CCCCCC"}}>
+            
             <div className="role" style={{marginTop:"20px", marginBottom:"30px"}}>
-              <div className="rolePhotos" style={{flexBasis:"40%", display:"flex", alignItems:"center", marginTop:"15px", marginBottom:"15px"}}>
-                <Carousel showThumbs={false} dynamicHeight={false} showArrows={true} emulateTouch={true} showStatus={false}>
-                  <div>
-                        <img src="./AsafTreeHacks.jpg" />
-                    </div>
-                    <div>
-                        <img src="./AsafTreeHacks.jpg" />
-                    </div>
-                    <div>
-                        <img src="./AsafTreeHacks.jpg" />
-                    </div>
-                    <div>
-                        <img src="./AsafTreeHacks.jpg" />
-                    </div>
-
-                </Carousel>
+              <div className="rolePhotos" style={{flexBasis:"45%", display:"flex", alignItems:"center"}}>
+                <div className="iframeWrapper" style={{position:"relative", width:"100%", height:"0", paddingBottom:"56.25%"}}>
+                  
+                </div>
               </div>
 
-              <div className="roleContent" style={{flexBasis:"55%"}}>
-                <h3 style={{fontSize:"24px", color:"#333333"}}>ILLUMIO</h3>
-                <p style={{fontFamily:"Helvetica-Bold", color:"#555555", fontSize:"18px"}}>Software engineer - distributed systems team</p>
-                <p style={{color:"#666666", fontSize:"18px"}}>I worked on many projects, from the core security policy computation engine, to automating our command line interface, to enforcing consistency and versioning of our APIs.</p>
-                <p style={{color:"#666666", fontSize:"18px"}}>On the side, I ran Illumio&#39;s university hackathon efforts at Berkeley and Stanford, and organized the company&#39;s first internal hackathon.</p>
+              <div className="roleContent" style={{flexBasis:"50%"}}>
+                <h3 style={{fontSize:"20px", color:"#333333"}}>Illumio</h3>
+                <p style={{fontFamily:"Helvetica-Bold", color:"#555555"}}>Software engineer - distributed systems team</p>
+                <p>When apple introduced 3d touch, I wanted to turn my iPhone into a functioning scale. When others&#39; attempts were rejected from the app store, I built a Flappy Bird like game that could be secretly used to do it.</p>
+                <p>After nearly a million video views and over 100,000 downloads in two days, I got a nice call from apple and the app and video had to go. Here is the video quietly re-uploaded!</p>
               </div>
-            </div>
-
-            <div className="role" style={{marginTop:"20px", marginBottom:"30px"}}>
               
-
-              <div className="roleContent" style={{flexBasis:"55%"}}>
-                <h3 style={{fontSize:"24px", color:"#333333"}}>ILLUMIO</h3>
-                <p style={{fontFamily:"Helvetica-Bold", color:"#555555", fontSize:"18px"}}>Software engineer - distributed systems team</p>
-                <p style={{color:"#666666", fontSize:"18px"}}>After nearly a million video views and over 100,000 downloads in two days, I got a nice call from apple and the app and video had to go. Here is the video quietly re-uploaded!</p>
-                <p style={{color:"#666666", fontSize:"18px"}}>After nearly a million video views and over 100,000 downloads in two days, I got a nice call from apple and the app and video had to go. Here is the video quietly re-uploaded!</p>
-              </div>
-
-              <div className="rolePhotos" style={{flexBasis:"40%", display:"flex", alignItems:"center", marginTop:"15px", marginBottom:"15px"}}>
-                <Carousel showThumbs={false} dynamicHeight={false} showArrows={true} emulateTouch={true} showStatus={false}>
-                  <div>
-                        <img src="./AsafTreeHacks.jpg" />
-                    </div>
-                    <div>
-                        <img src="./AsafTreeHacks.jpg" />
-                    </div>
-                    <div>
-                        <img src="./AsafTreeHacks.jpg" />
-                    </div>
-                    <div>
-                        <img src="./AsafTreeHacks.jpg" />
-                    </div>
-
-                </Carousel>
-              </div>
             </div>
-
-            <div className="role" style={{marginTop:"20px", marginBottom:"30px"}}>
-              <div className="rolePhotos" style={{flexBasis:"40%", display:"flex", alignItems:"center", marginTop:"15px", marginBottom:"15px"}}>
-                <Carousel showThumbs={false} dynamicHeight={false} showArrows={true} emulateTouch={true} showStatus={false}>
-                  <div>
-                        <img src="./AsafTreeHacks.jpg" />
-                    </div>
-                    <div>
-                        <img src="./AsafTreeHacks.jpg" />
-                    </div>
-                    <div>
-                        <img src="./AsafTreeHacks.jpg" />
-                    </div>
-                    <div>
-                        <img src="./AsafTreeHacks.jpg" />
-                    </div>
-
-                </Carousel>
-              </div>
-
-              <div className="roleContent" style={{flexBasis:"55%"}}>
-                <h3 style={{fontSize:"24px", color:"#333333"}}>ILLUMIO</h3>
-                <p style={{fontFamily:"Helvetica-Bold", color:"#555555", fontSize:"18px"}}>Software engineer - distributed systems team</p>
-                <p style={{color:"#666666", fontSize:"18px"}}>After nearly a million video views and over 100,000 downloads in two days, I got a nice call from apple and the app and video had to go. Here is the video quietly re-uploaded!</p>
-                <p style={{color:"#666666", fontSize:"18px"}}>After nearly a million video views and over 100,000 downloads in two days, I got a nice call from apple and the app and video had to go. Here is the video quietly re-uploaded!</p>
-              </div>
-            </div>
-
           </div>
 
-          <div className="footer" style={{height:"200px"}}>
+          <div className="contact" style={{height:"200px"}}>
           </div>
         </div>
       </div>
